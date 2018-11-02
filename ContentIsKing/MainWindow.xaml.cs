db04d7cbaf09bd894e2bfedb8f5f6d46d67e9bac
data <data@DESKTOP-3SQ0DD9>
2018-11-02 11:26:08 +0700

    target dotnet3.5  -> de trien khai

--- ContentIsKing/MainWindow.xaml.cs
+++ ContentIsKing/MainWindow.xaml.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
