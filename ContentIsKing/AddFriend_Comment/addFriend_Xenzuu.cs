6f11de0cb93dda906026fe08f0086db5730bdff7
data <data@DESKTOP-3SQ0DD9>
2018-11-22 17:59:41 +0700

    111

--- ContentIsKing/AddFriend_Comment/addFriend_Xenzuu.cs
+++ ContentIsKing/AddFriend_Comment/addFriend_Xenzuu.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using ContentIsKing.MasterK;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ContentIsKing.AddFriend_Comment
 {
@@ -9,7 +13,47 @@ namespace ContentIsKing.AddFriend_Comment
     {
         public static void addFriend_Xen()
         {
-            string url = "https://www.xenzuu.com/fl188228";
+            string user = "nguyenthuylinhls";
+            string pass = "cstd1234";
+            //login
+            var client = new RestClient("https://www.xenzuu.com/index.php");
+            var request = new RestRequest(Method.POST);
+            CookieContainer _cookieJar = new CookieContainer();
+            client.CookieContainer = _cookieJar;
+
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request.AddParameter("mp", "home");
+            request.AddParameter("ac", "login");
+            request.AddParameter("user", user);
+            request.AddParameter("pwd", pass);
+            request.AddParameter("autologin", "1");
+
+            IRestResponse response = client.Execute(request);
+
+
+            string html = response.Content;
+
+            // post
+            if (html.ToLower().Contains("logout"))
+            {
+
+                string url = "https://www.xenzuu.com/fl188228";
+                string html1 = GetDataFromUrl.getHTML(url);
+
+                MatchCollection contents = Regex.Matches(html1, "href='pr\\/(.*?)'(.*?)id_user=(\\d+)");
+      
+             
+                foreach (Match content in contents)
+                {
+                    foreach (Capture capture in content.Captures)
+                    {
+                        //string s = capture.Grou
+                    }
+                }
+            }
+
+            
         }
+       
     }
 }
